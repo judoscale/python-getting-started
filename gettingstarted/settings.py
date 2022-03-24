386d9862fc8fa584bab11c0e4506f5833208a8fd
Adam McCrea <adam@adamlogic.com>
2022-03-24 15:15:32 -0400

    Install judoscale-python

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "judoscale.django",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
